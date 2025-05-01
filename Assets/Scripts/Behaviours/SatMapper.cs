79c88d58affd04af8e45bf8492c819dba0f2fea5
ru1t3rl <lucaruiters@hotmail.nl>
2025-05-01 23:15:15 +0200

    Clean code and add update method

--- Assets/Scripts/Behaviours/SatMapper.cs
+++ Assets/Scripts/Behaviours/SatMapper.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using SatLight.Enums;
+using System.Linq;
 using SatLight.Models;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace SatLight.Behaviours
 {
     public class SatMapper : MonoBehaviour
     {
-        [SerializeField]
-        private SatGatherer satGatherer;
+        [SerializeField] private SatGatherer satGatherer;
 
-        [SerializeField]
-        private GameObject satellitePrefab;
+        [SerializeField] private GameObject satellitePrefab;
 
-        [SerializeField]
-        private Transform satellitesParent;
+        [SerializeField] private Transform satellitesParent;
 
-        [SerializeField]
-        private Vector3 distanceFromParent = new (0.1f, 0.1f, 0.1f);
+        [SerializeField] private Vector3 distanceFromParent = new(0.1f, 0.1f, 0.1f);
 
         private async void Awake()
         {
@@ -27,19 +22,55 @@ namespace SatLight.Behaviours
             InstantiateSatellites(satellites);
         }
 
-        private void InstantiateSatellites(SatAbove[] sats)
+        private void InstantiateSatellites(SatAbove[] satellites)
         {
-            foreach (var satellite in sats)
+            foreach (var satellite in satellites)
             {
-                Vector3 location = new Location(satellite.SatLat, satellite.SatLng, satellite.SatAlt)
-                    .ToUnityCoordinates((satellitesParent ?? transform).localScale + distanceFromParent, satellitesParent.localRotation);
-
-                var satGameObject = Instantiate(satellitePrefab, location, Quaternion.identity, satellitesParent ?? transform);
-                var satBehaviour = satGameObject.GetComponent<SatelliteBehaviour>() ??
-                                   satGameObject.AddComponent<SatelliteBehaviour>();
-                
-                satBehaviour?.SetSatelliteIno(satellite);
+                CreateSatelliteGameObject(satellite);
             }
+        }
+
+#if UNITY_EDITOR
+        [ContextMenu("Update Satellites")]
+#endif
+        private async void UpdateSatellites()
+        {
+            var satellites = await satGatherer.GetSatellites();
+
+            IEnumerable<SatelliteBehaviour> children = transform.GetComponentsInChildren<SatelliteBehaviour>();
+
+            foreach (var satellite in satellites)
+            {
+                SatelliteBehaviour? child = children.SingleOrDefault(c => c.gameObject.name == $"Sat_{satellite.SatId}");
+
+                if (!child)
+                {
+                    CreateSatelliteGameObject(satellite);
+                }
+                else
+                {
+                    var location = new Location(satellite.SatLat, satellite.SatLng, satellite.SatAlt);
+                    child.transform.localPosition = location.ToUnityCoordinates(
+                        (satellitesParent ?? transform).localScale + distanceFromParent,
+                        satellitesParent?.localRotation ?? Quaternion.identity);
+                    child.SetSatelliteIno(satellite);
+                }
+            }
+        }
+
+        private void CreateSatelliteGameObject(SatAbove satellite)
+        {
+            Vector3 location = new Location(satellite.SatLat, satellite.SatLng, satellite.SatAlt)
+                .ToUnityCoordinates((satellitesParent ?? transform).localScale + distanceFromParent,
+                    satellitesParent?.localRotation ?? Quaternion.identity);
+
+            var satGameObject =
+                Instantiate(satellitePrefab, location, Quaternion.identity, satellitesParent ?? transform);
+            var satBehaviour = satGameObject.GetComponent<SatelliteBehaviour>() ??
+                               satGameObject.AddComponent<SatelliteBehaviour>();
+
+            satGameObject.name = $"Sat_{satellite.SatId}";
+            satBehaviour?.SetSatelliteIno(satellite);
         }
     }
 }
