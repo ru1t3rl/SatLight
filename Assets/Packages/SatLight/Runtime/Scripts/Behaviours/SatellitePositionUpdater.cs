fe51aac2f297967eb470286e4c0b4b365ed81bb7
ru1t3rl <lucaruiters@hotmail.nl>
2025-05-18 16:00:29 +0200

    Improve satellite position updater by using the future positions queue

--- Assets/Packages/SatLight/Runtime/Scripts/Behaviours/SatellitePositionUpdater.cs
+++ Assets/Packages/SatLight/Runtime/Scripts/Behaviours/SatellitePositionUpdater.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using SatLight.Runtime.Domain.Common;
 using UnityEngine;
 
@@ -6,21 +9,50 @@ namespace SatLight.Runtime.Behaviours
     [RequireComponent(typeof(SatelliteData))]
     public class SatellitePositionUpdater : MonoBehaviour
     {
+        [SerializeField] private float updateRate = 1f; // The positions in FuturePositions are approximately 1sec apart
         private SatelliteData _data;
+        private CancellationTokenSource _tokenSource;
 
         private void Awake()
         {
             _data = GetComponent<SatelliteData>();
         }
 
-        private void FixedUpdate()
+        private void OnEnable()
         {
-            Vector3 direction = transform.localPosition.normalized;
-            
-            // TODO: Check if we need to fix anything altitude related
-            transform.position = _data.Location.ToUnityCoordinates(
-                direction * (float)(transform.parent.localScale.magnitude + _data.SatInfo.SatAlt) 
-            );
+            if (_tokenSource.IsCancellationRequested)
+            {
+                _tokenSource.Dispose();
+                _tokenSource = new CancellationTokenSource();
+            }
+
+            _ = UpdateSatellitePosition(_tokenSource.Token);
+        }
+
+        private void OnDisable()
+        {
+            _tokenSource.Cancel();
+        }
+
+        private void OnDestroy()
+        {
+            _tokenSource.Dispose();
+        }
+
+        private async Task UpdateSatellitePosition(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay((int)(updateRate * 1000), token);
+
+                Location nextLocation = _data.GetNextLocation();
+                _data.UpdateCurrentLocation(nextLocation.Latitude, nextLocation.Longitude, nextLocation.Altitude);
+                
+                Vector3 direction = transform.localPosition.normalized;
+                transform.position = _data.Location.ToUnityCoordinates(
+                    direction * (float)(transform.parent.localScale.magnitude + _data.SatInfo.SatAlt)
+                );
+            }
         }
     }
 }
